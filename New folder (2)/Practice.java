d08d51a51ce46d63ba91df4b5bfb04c9b733d86e
Vaibhav Davang <81429122+vaibhavdavang1998@users.noreply.github.com>
2023-01-02 19:30:54 +0530

    Add files via upload

---
+++ New folder (2)/Practice.java
@@ -0,0 +1,11 @@
+
+public class Practice {
+    static void telljoke()//static words  se aap methods ya function ko bina object banaye call kar sakte ho for ex man lo ek hostel he or 
+    //usme bohat sare bacche rahte he apne apne room me aap fridge 2 prakar se de sakte ho ek to ek ek ko  fridge 100 baccho ko or dusra ki hostel ke baher rakh kar sabke liye ek
+    {
+        System.out.println("i inveted new word");
+    }
+    public static void main(String[] args) {
+        telljoke();
+    }
+}
